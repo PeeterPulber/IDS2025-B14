2470df83ba22bfdc0cc36a2bb8998545e6196e65
Uku Viispert <uku.viispert@gmail.com>
2025-12-01 16:07:29 +0200

    tweaks

--- scrape.py
+++ scrape.py
@@ -2,16 +2,19 @@ import nest_asyncio
 nest_asyncio.apply()
 from playwright.sync_api import sync_playwright
 
+with sync_playwright() as pw:
+    firefox = pw.firefox.launch(headless=False)
+    page = firefox.new_page()
+    url = "https://www.metal-archives.com/bands/Brad_Jurjens/3540460030"
+    page.goto(url)
+    page.wait_for_selector("table.display.discog tbody")
 
-pw = sync_playwright().start()
-firefox = pw.firefox.launch(headless=False)
-page = firefox.new_page()
-page.goto("https://www.metal-archives.com/bands/Aeon_Aethereal/3540258444")
-page.wait_for_selector("dl.float_right dd")
+    genre = page.query_selector("dl.float_right dd").inner_text()
+    formed = page.query_selector("dl.float_left dd:nth-of-type(4)").inner_text()
+    years = page.query_selector("dl.clear dd").inner_text()
+    location = page.query_selector("dl.float_left dd:nth-of-type(2)").inner_text()
 
-genre = page.query_selector("dl.float_right dd").inner_text()
-years = page.query_selector("dl.clear dd").inner_text()
-location = page.query_selector("dl.float_left dd:nth-of-type(2)").inner_text()
 print(genre)
+print(formed)
 print(years)
 print(location)
