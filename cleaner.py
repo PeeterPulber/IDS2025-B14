289d5ad28f7959c2a380831718472f39f947dc7f
Uku Viispert <uku.viispert@gmail.com>
2025-12-04 13:06:03 +0200

    Fix genre cleaning and start work on time period data

--- cleaner.py
+++ cleaner.py
@@ -8,15 +8,15 @@ filename = "bandsEE.jsonl"
 df = pd.read_json(filename, lines=True)
 cleandf = df.copy()
 
-#for r in cleandf["genre"]:
-
+# Clean the genre column
 cleandf["genre"] = cleandf["genre"].str.replace(" Metal", "")
-cleandf["genre"] = cleandf["genre"].str.replace("(early)", "")
-cleandf["genre"] = cleandf["genre"].str.replace("(later)", "")
+cleandf["genre"] = cleandf["genre"].str.replace(" (early)", "")
+cleandf["genre"] = cleandf["genre"].str.replace(" (later)", "")
 cleandf["genre"] = cleandf["genre"].str.replace(";", "/")
 
-cleandf["genre"] = cleandf["genre"].str.split("/")
+cleandf["genre"] = cleandf["genre"].str.split(r"\s*/\s*")
 
+# Vectorize genres
 mlb = MultiLabelBinarizer()
 genres = mlb.fit_transform(cleandf['genre'])
 genres = pd.DataFrame(genres, columns=mlb.classes_, index=cleandf.index)
@@ -24,4 +24,7 @@ genres = pd.DataFrame(genres, columns=mlb.classes_, index=cleandf.index)
 cleandf = cleandf.drop('genre',axis = 1)
 cleandf = cleandf.join(genres)
 
-print(cleandf.head())
+# Set year of formation as the year of the first release for bands without a year of formation
+cleandf["firstrelease"] = cleandf["releases"].apply(lambda rels: rels[0] if len(rels) > 0 else None)
+mask = cleandf["formed"] == "N/A"
+cleandf.loc[mask, "formed"] = cleandf.loc[mask, "firstrelease"]
