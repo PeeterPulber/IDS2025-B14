2ff88bc440564133c5af22c6aa778468cc3adffd
Uku Viispert <uku.viispert@gmail.com>
2025-12-04 16:39:42 +0200

    Parse years

--- cleaner.py
+++ cleaner.py
@@ -1,7 +1,49 @@
 import pandas as pd
 import sklearn
+import re
+from datetime import date
 from sklearn.preprocessing import OneHotEncoder, MultiLabelBinarizer
 
+def parse_years(r):
+    # Bands are assumed to have been active during years when they released music
+    activity = set(r["releases"])
+    cleaned = r["years"]
+
+    if cleaned != "N/A" and cleaned != "?":
+        # Get rid of text in parentheses
+        cleaned = re.sub(r"\s\(.*\)", "", cleaned)
+        cleaned = cleaned.replace("?", "").split(",")
+        # Periods of activity
+
+        for p in cleaned:
+            p = p.strip().split("-")
+            # Extract years in each period
+            if len(p) == 1: # One year
+                if p[0].isdigit():
+                    activity.add(int(p[0]))
+                    continue
+                else:
+                    continue
+            elif p[0] == "" and p[1] == "":
+                continue
+            elif p[0] == "":
+                activity.add(int(p[1]))
+                continue
+            elif p[1] == "":
+                activity.add(int(p[0]))
+                continue
+            elif p[1] == "present":
+                end = int(date.today().year)
+                start = int(p[0])
+            else:
+                end = int(p[1])
+                start = int(p[0])
+            # Add years to activity set
+            while start <= end:
+                activity.add(end)
+                end -= 1
+
+    return activity
 
 filename = "bandsEE.jsonl"
 
@@ -13,7 +55,7 @@ cleandf["genre"] = cleandf["genre"].str.replace(" Metal", "")
 cleandf["genre"] = cleandf["genre"].str.replace(" (early)", "")
 cleandf["genre"] = cleandf["genre"].str.replace(" (later)", "")
 cleandf["genre"] = cleandf["genre"].str.replace(";", "/")
-
+# Split genres at "/" and remove any neighbouring spaces
 cleandf["genre"] = cleandf["genre"].str.split(r"\s*/\s*")
 
 # Vectorize genres
@@ -28,3 +70,11 @@ cleandf = cleandf.join(genres)
 cleandf["firstrelease"] = cleandf["releases"].apply(lambda rels: rels[0] if len(rels) > 0 else None)
 mask = cleandf["formed"] == "N/A"
 cleandf.loc[mask, "formed"] = cleandf.loc[mask, "firstrelease"]
+# Drop bands for which there is no information about their period of activity
+cleandf = cleandf.dropna(subset=["formed"])
+
+# Clean the years string
+cleandf["lastrelease"] = cleandf["releases"].apply(lambda rels: rels[-1] if len(rels) > 0 else None)
+cleandf["years"] = cleandf.apply(parse_years, axis=1)
+
+print(cleandf["years"])
